93722e2c3b532ec327674d7635a6059840873ef0
MyeongJunKim <myeongjun222@naver.com>
2023-06-30 16:02:40 +0900

    [feat] 토큰 만료시간 확인및 처리 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+
 import RootRouter from "./RootRouter/RootRouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import Error from "./pages/Error/Error";
 
 function App() {
-  return <RootRouter />;
+  return (
+    <ErrorBoundary fallback={<Error />}>
+      <BrowserRouter>
+        <RootRouter />
+      </BrowserRouter>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
--- src/RootRouter/RootRouter.tsx
+++ src/RootRouter/RootRouter.tsx
@@ -1,25 +1,29 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+
 import SignIn from "../pages/SignIn/SignIn";
 import SignUp from "../pages/SignUp/SignUp";
 import TodoList from "../pages/TodoList/TodoList";
 import Index from "../pages/Index/Index";
 import NotFound from "../pages/Error/NotFound";
-import ErrorBoundary from "../components/ErrorBoundary";
-import Error from "../pages/Error/Error";
+import useAuth from "../hooks/useAuth";
 
 function RootRouter() {
+  const { authChecker } = useAuth();
+
+  useEffect(() => {
+    authChecker();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <ErrorBoundary fallback={<Error />}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/todo" element={<TodoList />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </ErrorBoundary>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/todo" element={<TodoList />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
