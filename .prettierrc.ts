07f839832a7794783b84bd4e8fbf410c25aa1571
ggnsall <ggnsall@naver.com>
2023-06-30 12:27:50 +0900

    [feat] prettier, eslint, husky 적용

---
+++ .prettierrc.ts
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 90, // 한 줄에 최대 갯수
+  tabWidth: 2, // 탭 너비
+  bracketSameLine: true, // HTML 태그시 닫는 모양 > 같은 라인으로
+  arrowParens: "avoid", // x => x 가능
+  trailingComma: "none", // 객체 마지막 요소 , 없애기
+};
